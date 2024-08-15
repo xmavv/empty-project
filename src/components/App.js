92c8935b6c55b415d0ce53fe976ce252fe697274
xmavv <xxmatheooo@gmail.com>
2024-08-15 02:18:33 +0200

    added more semantic names

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,26 @@ import NoteInput from "./NoteInput";
 import Toggle from "./Toggle";
 import MyToaster from "./MyToaster";
 import KeyInstructions from "./KeyInstructions";
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
-import {useDark} from "../hooks/useDark";
-import {useNote} from "../contexts/NoteContext";
+import { useDark } from "../hooks/useDark";
+import { useNote } from "../contexts/NoteContext";
 
 export default function App() {
   const { isDark } = useNote();
-  useDark( isDark );
+  useDark(isDark);
 
   return (
-    <div className={ styles.app } data-theme={ isDark ? "dark" : "light" }>
-      <div className={ styles.container }>
+    <main className={styles.app} data-theme={isDark ? "dark" : "light"}>
+      <div className={styles.container}>
         <NoteList />
         <NoteInput />
       </div>
       <MyToaster />
-      <KeyInstructions />
-      <Toggle />
-    </div>
+      <footer>
+        <KeyInstructions />
+        <Toggle />
+      </footer>
+    </main>
   );
 }
